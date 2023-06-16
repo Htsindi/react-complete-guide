7e24fe6a44224f19c35a344ddaaa30185a902527
Martin <matefortechnology@gmail.com>
2023-06-16 21:35:22 +0200

    PassData
    pass data from using pointer from child component to parent

--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 import "./NewExpenseForm.css";
 
-const NewExpenseForm = () => {
-  /*///Simpler Option and Favoured
+const NewExpenseForm = (props) => {
+  ///Simpler Option and Favoured
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -17,8 +17,8 @@ const NewExpenseForm = () => {
   const dateChangeHandler = (e) => {
     setEnteredDate(e.target.value);
   };
-  */
-  ///Option for restructuring ensure no data is lost by destructuring ...
+  /*
+  ///Option for restructuring ensure no data is lost by destructuring and will get the latest state snapshop...
   const [userInput, setUserInput] = useState({
     enteredTitle: "",
     enteredAmount: "",
@@ -27,33 +27,69 @@ const NewExpenseForm = () => {
 
   const titleChangeHandler = (e) => {
     setUserInput((prevState) => {
-      return { ...userInput, setEnteredTitle: e.target.value };
+      return { 
+        ...prevState, enteredTitle: e.target.value };
     });
   };
 
   const amountChangeHandler = (e) => {
     setUserInput((prevState) => {
       return {
-        ...userInput,
-        setEnteredAmount: e.target.value,
+        ...prevState,
+        enteredAmount: e.target.value,
       };
     });
   };
   const dateChangeHandler = (e) => {
     setUserInput((prevState) => {
       return {
-        ...userInput,
-        setEnteredDate: e.target.value,
+        ...prevState,
+        enteredDate: e.target.value,
       };
     });
   };
+*/
+  /*/Another Option
+const eventHandler = (identifier, value)=>{
+  if (identifier === 'title'){
+    setEnteredTitle(value);
+  }else if(identifier === 'amount'){
+    setEnteredAmount(value);
+  } else{
+    setEnteredDate(value);
+  };
+}*/
+  //submission of form when submit button is clicked
+  const submitHandler = (e) => {
+    //to prevent the browser reload
+    e.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    //console.log(expenseData);
+    //from newExpense
+    props.onSaveExpenseData(expenseData);
+    //Two way binding working with value on input form to clear data on form
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  // for Optopn 3 /*<input type="text" value={enteredTitle} onChange={(e)=>eventHandler('title', e.target.value)} />*/
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -61,6 +97,7 @@ const NewExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -70,6 +107,7 @@ const NewExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2023-06-30"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
