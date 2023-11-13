a5d2dd01d2f3fd76ed51848933d2b37f44604efd
Martin <matefortechnology@gmail.com>
2023-11-13 21:37:56 +0200

    filtering data
    still to display

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,22 +8,25 @@ import "./Expenses.css";
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2022");
 
-  let filterInfoText = "2019, 2020 & 2021";
+  //let filterInfoText = "2019, 2020 & 2021";
 
-  if (filteredYear === "2019") {
-    filterInfoText = "2020, 2021, & 2022";
-  } else if (filteredYear === "2020") {
-    filterInfoText = "2019, 2021, & 2022";
-  } else if (filteredYear === "2021") {
-    filterInfoText = "2019, 2020, & 2022";
-  } else {
-    filterInfoText = "2019, 2020, & 2021";
-  }
+  //if (filteredYear === "2019") {
+  //filterInfoText = "2020, 2021, & 2022";
+  //} else if (filteredYear === "2020") {
+  //filterInfoText = "2019, 2021, & 2022";
+  //} else if (filteredYear === "2021") {
+  //filterInfoText = "2019, 2020, & 2022";
+  //} else {
+  //filterInfoText = "2019, 2020, & 2021";}
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     //console.log("Expenses.js");
-    //console.log(selectedYear);
+    console.log(selectedYear);
+    const filteredItems = props.items.filter(
+      (item) => item.date.getFullYear() === Number(selectedYear)
+    );
+    console.log(filteredItems);
   };
 
   return (
@@ -32,27 +35,16 @@ function Expenses(props) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <p> Data for years {filterInfoText} is hidden. </p>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+
+      {props.items.map((expense) => (
+        <ExpenseItem
+          //Shld always add a key when using map
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 }
